bd140834e715b2e0761736a4d36f2fa84531638a
heavyha <100425053+heavyha@users.noreply.github.com>
2024-04-29 15:31:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Zxilly/UA2F.git
+git clone https://github.com/CHN-beta/rkp-ipid.git
